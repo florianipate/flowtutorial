83b33ddaca5344cf1f1712381d826ccd44cf3f51
Florian Ipate <florian.ipate@gmail.com>
2022-09-04 13:15:50 +0100

    lwc update

--- force-app/main/default/lwc/richTextAteaLWC/richTextAteaLWC.js
+++ force-app/main/default/lwc/richTextAteaLWC/richTextAteaLWC.js
@@ -1,3 +1,48 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class RichTextAteaLWC extends LightningElement {}
+import {
+    FlowAttributeChangeEvent,
+    FlowNavigationNextEvent,
+    FlowNavigationBackEvent,
+    FlowNavigationPauseEvent,
+    lowNavigationFinishEvent
+} from 'lightning/flowSupport';
+
+export default class RichTextAteaLWC extends LightningElement {
+    @api placeholder;
+    @api required;
+    @api value;
+    @api label;
+    requiredMessage = 'Value is required for this field';
+    _validity = true;
+
+    handleChange = (event) =>{
+        event.preventDefault();
+        event.value = event.target.value;
+
+    }
+
+    @api
+    validate(){
+        if(this.validateInput()){
+            return{
+                isValid: true
+            };
+        } else {
+            return{
+                isValid: false,
+                errorMessage: this.requiredMessage
+            };
+        }
+
+    }
+    
+    validateInput(){
+        if(!this.value){
+            this._validity = false;
+        } else{
+            this._validity = true;
+        }
+        return this._validity;
+    }
+}
