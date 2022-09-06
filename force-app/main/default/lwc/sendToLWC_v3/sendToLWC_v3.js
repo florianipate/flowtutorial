4c36ba77725e95ade2db87d1f2833b3825bf24a2
Florian Ipate <florian.ipate@gmail.com>
2022-09-06 23:02:22 +0100

    vf to lwc

---
+++ force-app/main/default/lwc/sendToLWC_v3/sendToLWC_v3.js
@@ -0,0 +1,21 @@
+import { LightningElement } from 'lwc';
+
+export default class SendToLWC_v3 extends LightningElement {
+    messageFromVF;
+    
+  connectedCallback() {
+    var VfOrigin =
+      "https://adovys-f-dev-ed--c.vf.force.com/apex/SampleVFtoLWC";
+    window.addEventListener("message", (message) => {
+    //   if (message.origin !== VfOrigin) {
+    //     //Not the expected origin
+    //     return;
+    //   }
+
+      //handle the message
+    //   if (message.data.name === "SampleVFToLWCMessage") {
+        this.messageFromVF = JSON.parse(JSON.stringify(message.data));
+    //   }
+    });
+  }
+}
